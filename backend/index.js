41cadf32f73026d690d506d932461407cfbaa673
NoahKoh <kohyusheng@gmail.com>
2024-12-05 23:03:27 +0800

    added the get method to select data from mysql database display it

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import mysql from "mysql2"
+import cors from "cors"
 
 const app = express() // Express to handle requests
 
@@ -10,8 +11,19 @@ const db = mysql.createConnection({ // Connect to the database
     database:"test"
 })
 
+app.use(express.json()) // Middleware to parse JSON
+app.use(cors()) // Muddleware for Cross-Origin Resource Sharing. In order to interact with backend.
+
 app.get("/", (req, res)=> {
     res.send("test")
+})
+
+app.get("/items", (req, res)=> { {/* /items is the endpoint for express server. The items table */}
+    const q = "SELECT * FROM items"
+    db.query(q, (err, data) => {
+        if (err) {return res.json(err)}
+        return res.json(data)
+    })
 })
 
 app.listen(8800, ()=> {
--- client/src/Pages/Items.jsx
+++ client/src/Pages/Items.jsx
@@ -1,8 +1,39 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios" // Use to fetch data from the backend servers
 
 const Items = () => {
+    const [items, setItems] = useState([]) // Use to get and Set an array of items
+
+    useEffect(()=>{ // Used to run once to fetch items data
+        const fetchData = async () => {
+            try{
+                const res = await axios.get("http://localhost:8800/items")
+                setItems(res.data)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    }, [])
+
     return (
-        <div>Items</div>
+        <div>
+            <h1>Item Record</h1>
+            <div className="items">
+                {items.map(item=>(
+                    <div className="item" key={item.id}>
+                        <h2>{item.name}</h2>
+                        <p>{item.desc}</p>
+                        <span>{item.info}</span>
+                        <span>{item.extrainfo}</span>
+                    </div>
+                ))}
+            </div>
+            <button className="adding">
+                <Link to="/add">Add new Item</Link>
+            </button>
+        </div>
     )
 }
 
